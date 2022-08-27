8d133a6899b7ae69e9e9a1f0e8b6273ef80a5fb1
valeryO <olennval@mail.ru>
2022-08-27 18:38:34 +0700

    nuber 47

---
+++ z47/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите размерность матрицы");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double [,] matrix = new double [m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(-100, 100);
+        Console.Write(matrix[i,j]+" ");
+    }
+Console.WriteLine();
+}
