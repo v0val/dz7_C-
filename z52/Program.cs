5983459ad1a0eb38d289424baea06359c3c0e50d
valeryO <olennval@mail.ru>
2022-08-27 19:08:41 +0700

    number 52

---
+++ z52/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите размерность матрицы");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int [m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(- 100, 100);
+        Console.Write(matrix[i,j]+" ");
+        
+    }
+Console.WriteLine();
+}
+double summa = 0;
+for (int j = 0; j < n; j++)
+{
+    summa = 0;
+    for (int i = 0; i < m; i++)
+    {
+        summa += matrix[i,j];
+    }
+Console.Write($"среднее арифметическое элементов в столбце {j} = ");
+Console.WriteLine(summa/m);
+}
